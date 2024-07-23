d32f1b642e7c8543aea8c8401987d43b47e7c0ed
FalsePhilosopher <108245223+FalsePhilosopher@users.noreply.github.com>
2024-07-23 16:03:33 -0600

    Create trim.bat

---
+++ trim.bat
@@ -0,0 +1,16 @@
+Echo off
+ 
+set isodir=.\%PWD%
+
+set converter=.\gcit.exe
+ 
+if NOT exist "%isodir%" (
+  echo "ERROR directory doesn't exist"
+  goto :EOF
+)
+ 
+ 
+FOR /F "tokens=1 delims=." %%A IN ('dir /b "%isodir%"') DO (
+  echo converting: %%A
+  "%converter%" "%isodir%\%%A.iso" -b -d "%isodir%\converted\%%A.iso" -AQ
+)
